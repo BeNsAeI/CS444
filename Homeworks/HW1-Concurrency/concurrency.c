8f83152dff8558cb2308533a9ddd89a37185df50
Levi Willmeth <levi.willmeth@gmail.com>
2017-04-18 11:42:21 -0700

    Update concurrency, add McGrath's library instead.

--- Homeworks/HW1-Concurrency/concurrency.c
+++ Homeworks/HW1-Concurrency/concurrency.c
@@ -4,18 +4,18 @@
   Behnam Saeedi, Levi Willmeth, Zhaoheng Wang
 */
 
+#include <unistd.h>
+#include "mt19937ar.c"
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include "mt.h"
 
 pthread_mutex_t bufferLock;
 
 struct bufferItem{
-	long tid;
-  long number;
-	long sleep_time;
+	int tid;
+  int number;
+	int sleep_time;
 };
 
 struct bufferItem buffer[32];
@@ -24,17 +24,17 @@ int bufferIndex = 0;
 void *producer(void *tid){
   printf("Starting producer thread..\n");
   for(int i=0; i<3; i++){
-    long rNum = genrand_int32();
-    long sleep = genrand_int32()%7+2; // consumer sleeps 2-9 sec
-    struct bufferItem new = {i, rNum, sleep};
-    printf("Producer (%ld) sleeping..\n", sleep);
-    sleep(genrand_int32()%3+4); // producer sleeps 3-7 seconds
-    printf("Producer (%ld) waking up..\n");
+    int rand_num = genrand_int32();
+    int sleep_time = genrand_int32()%7+2; // consumer sleeps 2-9 sec
+    struct bufferItem new = {i, rand_num, sleep_time};
+    printf("Producer (%d) sleeping for %d seconds.\n", i, sleep_time);
+    sleep(genrand_int32() % 3+4); // producer sleeps 3-7 seconds
+    printf("Producer (%d) waking up..\n", i);
     pthread_mutex_lock(&bufferLock);
-    printf("Producer (%ld) has the buffer.\n");
+    printf("Producer (%d) has the buffer.\n", i);
 
     pthread_mutex_unlock(&bufferLock);
-    printf("Producer (%ld) relenquished the buffer.\n");
+    printf("Producer (%d) relenquished the buffer.\n", i);
   }
 }
 
