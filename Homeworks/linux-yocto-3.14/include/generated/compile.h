0cab775e60fdedbd625fee88b26424893a206bd4
Levi Willmeth <levi.willmeth@gmail.com>
2017-05-22 21:53:11 -0700

    Not sure if we need these..

--- Homeworks/linux-yocto-3.14/include/generated/compile.h
+++ Homeworks/linux-yocto-3.14/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 30 */
+/* This file is auto generated, version 35 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "i386"
-#define UTS_VERSION "#30 SMP PREEMPT Sun May 21 14:13:35 PDT 2017"
+#define UTS_VERSION "#35 SMP PREEMPT Mon May 22 20:50:40 PDT 2017"
 #define LINUX_COMPILE_BY "saeedib"
 #define LINUX_COMPILE_HOST "os-class.engr.oregonstate.edu"
 #define LINUX_COMPILER "gcc version 4.9.1 (GCC) "
