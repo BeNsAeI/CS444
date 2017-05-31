e3b837d743e033800f51eb7deac91380caaaa3da
Levi Willmeth <levi.willmeth@gmail.com>
2017-05-31 14:34:07 -0700

    Clean out previously deleted files.

--- Homeworks/linux-yocto-3.14/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-source /scratch/opt/environment-setup-i586-poky-linux
-
-qemu-system-i386 -gdb tcp::6503 -S -nographic -kernel ./arch/x86/boot/bzImage  -drive file=core-image-lsb-sdk-qemux86.ext3,if=virtio -enable-kvm -net none -usb -localtime --no-reboot --append "root=/dev/vda rw console=ttyS0 debug"
